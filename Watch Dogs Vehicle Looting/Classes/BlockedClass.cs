07dac3596ca07963eb0d413b0556b310c2a01d69
Kye Fenton <kyedylanfenton@gmail.com>
2023-11-05 02:47:51 +1030

    Quickly fix indentation

--- Watch Dogs Vehicle Looting/Classes/BlockedClass.cs
+++ Watch Dogs Vehicle Looting/Classes/BlockedClass.cs
@@ -5,6 +5,6 @@ namespace Watch_Dogs_Vehicle_Looting.Classes
 	public class BlockedClass
 	{
 		public string className { get; set; }
-        public List<string> modelExceptions { get; set; }
+		public List<string> modelExceptions { get; set; }
 	}
 }
